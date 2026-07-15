460816f6e4ccbe7fc8fac76b3f829c082d9ac662
shasulmedia-ghub <shasul.media@gmail.com>
2026-07-15 19:33:21 +0800

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Welcome from "./components/Welcome"
+import About from "./components/About"
+import Home from "./components/Home"
 import "./App.css"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
     <>
-    <h1>App Component</h1>
-    <Welcome/>
+<BrowserRouter>
+<nav>
+  <Link to="/gh-live-react/">Welcome</Link> |&nbsp;
+<Link to="/gh-live-react/about">About</Link> |&nbsp;
+<Link to="/gh-live-react/home">Home</Link>
+</nav>
+<Routes>
+  <Route path="gh-live-react/" element={<Welcome/>}> 
+    <Route path="gh-live-react/about" element={<About/>}/>
+    <Route path="gh-live-react/home" element={<Home/>}/>
+  </Route>
+</Routes>
+</BrowserRouter>
     </>
   )
 }
